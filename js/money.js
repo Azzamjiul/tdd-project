84a6baa07e5cf9be9e7ae75e7ad47e21c83b2d4c
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2022-09-09 15:24:43 +0700

    refactor: created Money and Portofolio modules; improved tests

---
+++ js/money.js
@@ -0,0 +1,16 @@
+class Money {
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    times(multiplier){
+        return new Money( this.amount * multiplier, this.currency );
+    }
+
+    divide(multiplier){
+        return new Money( this.amount / multiplier, this.currency );
+    }
+}
+
+module.exports = Money;
